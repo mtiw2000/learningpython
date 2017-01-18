92d6f971d9f79d1c53c14379792d55e08deb3221
mtiw2000 <mtiw2000@gmail.com>
2017-01-18 15:34:41 -0500

    second  commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 13:33:21 2017
+
+@author: manish
+"""
+
+from app import app
+import views
+
+
+if __name__ == '__main__':
+    app.run()
+    
+
---
+++ app/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 13:49:17 2017
+
+@author: manish
+"""
+from app import app
+
+@app.route('/')
+def homepage():
+    return 'Home page'
+    
