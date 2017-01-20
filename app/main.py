bc8e2682d96bb64d5f777387b62beba71eeafdc8
mtiw2000 <mtiw2000@gmail.com>
2017-01-20 17:30:38 -0500

    test

--- app/main.py
+++ app/main.py
@@ -12,4 +12,5 @@ import views
 if __name__ == '__main__':
     app.run()
     
+    
 
--- app/views.py
+++ app/views.py
@@ -5,8 +5,34 @@ Created on Tue Jan 17 13:49:17 2017
 @author: manish
 """
 from app import app
+import feedparser
+
+
 
 @app.route('/')
 def homepage():
     return 'Home page'
+    
+
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
+
+
+
+@app.route("/bbc")
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+        <body>
+            <h1> BBC Headlines </h1>
+            <b>{0}</b>  <br/>
+            <i>{1}</i> <br/>
+            <p>{2}</p>  <br/>
+        </body>
+    </html>""".format(first_article.get("title"), first_article.get("published"),first_article.get("summary"))
+
+        
+    
+    
     
